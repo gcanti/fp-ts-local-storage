5cb69355299824250428bc60768b3e57efb8612a
gcanti <giulio.canti@gmail.com>
2020-04-08 10:59:58 +0200

    document API unsafety, ref #6

--- README.md
+++ README.md
@@ -4,12 +4,35 @@
 
 - [API Reference](https://gcanti.github.io/fp-ts-local-storage)
 
-# Example
+**Example**
+
+```ts
+import * as assert from 'assert'
+import { setItem, getItem } from 'fp-ts-local-storage'
+import { some } from 'fp-ts/lib/Option'
+import { pipe } from 'fp-ts/lib/pipeable'
+import { chain } from 'fp-ts/lib/IO'
+
+// const program: IO<Option<string>>
+const program = pipe(
+  setItem('foo', JSON.stringify({ bar: 'baz' })),
+  chain(() => getItem('foo'))
+)
+
+assert.deepStrictEqual(program(), some('{"bar":"baz"}'))
+```
+
+Note that `localStorage` may throw, for example if disabled by the user or with a `QuotaExceededError`, so you may want to wrap the API call with a `tryCatch`
 
 ```ts
 import { setItem, getItem } from 'fp-ts-local-storage'
+import { chain, tryCatch } from 'fp-ts/lib/IOEither'
+import { pipe } from 'fp-ts/lib/pipeable'
+import { toError } from 'fp-ts/lib/Either'
 
-const program = setItem('foo', JSON.stringify({ bar: 'baz' })).chain(() => getItem('foo'))
-
-console.log(program.run()) // => some('{"bar":"baz"}')
+// const program: IOEither<Error, Option<string>>
+const program = pipe(
+  tryCatch(setItem('foo', JSON.stringify({ bar: 'baz' })), toError),
+  chain(() => tryCatch(getItem('foo'), toError))
+)
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,48 @@
 /**
+ * `fp-ts` bindings for `LocalStorage`
+ *
+ * **Example**
+ *
+ * ```ts
+ * import * as assert from 'assert'
+ * import { setItem, getItem } from 'fp-ts-local-storage'
+ * import { some } from 'fp-ts/lib/Option'
+ * import { pipe } from 'fp-ts/lib/pipeable'
+ * import { chain } from 'fp-ts/lib/IO'
+ *
+ * // const program: IO<Option<string>>
+ * const program = pipe(
+ *   setItem('foo', JSON.stringify({ bar: 'baz' })),
+ *   chain(() => getItem('foo'))
+ * )
+ *
+ * assert.deepStrictEqual(program(), some('{"bar":"baz"}'))
+ * ```
+ *
+ * Note that `localStorage` may throw, for example if disabled by the user or with a `QuotaExceededError`, so you may want to wrap the API call with a `tryCatch`
+ *
+ * ```ts
+ * import { setItem, getItem } from 'fp-ts-local-storage'
+ * import { chain, tryCatch } from 'fp-ts/lib/IOEither'
+ * import { pipe } from 'fp-ts/lib/pipeable'
+ * import { toError } from 'fp-ts/lib/Either'
+ *
+ * // const program: IOEither<Error, Option<string>>
+ * const program = pipe(
+ *   tryCatch(setItem('foo', JSON.stringify({ bar: 'baz' })), toError),
+ *   chain(() => tryCatch(getItem('foo'), toError))
+ * )
+ * ```
+ *
  * @since 0.2.0
  */
 import { IO } from 'fp-ts/lib/IO'
 import { fromNullable, Option } from 'fp-ts/lib/Option'
 
-const _clear = () => localStorage.clear()
-
 /**
  * @since 0.2.0
  */
-export const clear: IO<void> = _clear
+export const clear: IO<void> = () => localStorage.clear()
 
 /**
  * @since 0.2.0
@@ -25,12 +58,10 @@ export function key(index: number): IO<Option<string>> {
   return () => fromNullable(localStorage.key(index))
 }
 
-const _length = () => localStorage.length
-
 /**
  * @since 0.2.0
  */
-export const length: IO<number> = _length
+export const length: IO<number> = () => localStorage.length
 
 /**
  * @since 0.2.0
